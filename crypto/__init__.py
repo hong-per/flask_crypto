a6766096a56026f88a302ad0650dc1d863ceca5b
Sawon Hong <sawon_hongspice-factory.co.jp@SawonnoMacBook-Pro.local>
2022-05-22 23:05:08 +0900

    skeleton

---
+++ crypto/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from .views import main_views
+
+import config
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(config)
+
+    # ORM
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # Blueprint
+    app.register_blueprint(main_views.bp)
+
+    return app
