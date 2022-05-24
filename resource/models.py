916a1d3d8dff0a3432f277cbd8296ba49eb26177
hong-per <nunji926@gmail.com>
2022-05-25 00:17:59 +0900

    create models

---
+++ resource/models.py
@@ -0,0 +1,38 @@
+from resource import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False, unique=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(), nullable=False)
+
+
+class Region(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False, unique=True)
+
+
+class Server(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    host = db.Column(db.String(100), nullable=False, unique=True)
+    cpu = db.Column(db.Integer(), nullable=False)
+    memory = db.Column(db.Integer(), nullable=False)
+    instance = db.Column(db.Integer(), nullable=False)
+
+
+region_server = db.Table(
+    'region_server',
+    db.Column('region_id', db.Integer, db.ForeignKey('region.id', ondelete='CASCADE'), primary_key=True),
+    db.Column('server_id', db.Integer, db.ForeignKey('server.id', ondelete='CASCADE'), primary_key=True)
+)
+
+
+class Usage(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    server_id = db.Column(db.Integer(), db.ForeignKey(
+        'server.id'), ondelete='CASCADE')
+    record_date = db.Column(db.DateTime(), nullable=False)
+    cpu_usage = db.Column(db.Integer(), nullable=False)
+    memory_usage = db.Column(db.Integer(), nullable=False)
+    instance_usage = db.Column(db.Integer(), nullable=False)
