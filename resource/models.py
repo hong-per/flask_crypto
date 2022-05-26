f876c41ed96cb1298d809144bbec5689c33bfcec
hong-per <nunji926@gmail.com>
2022-05-27 00:13:59 +0900

    models revise

--- resource/models.py
+++ resource/models.py
@@ -23,16 +23,21 @@ class Server(db.Model):
 
 region_server = db.Table(
     'region_server',
-    db.Column('region_id', db.Integer, db.ForeignKey('region.id', ondelete='CASCADE'), primary_key=True),
-    db.Column('server_id', db.Integer, db.ForeignKey('server.id', ondelete='CASCADE'), primary_key=True)
+    db.Column('region_id', db.Integer, db.ForeignKey(
+        'region.id', ondelete='CASCADE'), primary_key=True),
+    db.Column('server_id', db.Integer, db.ForeignKey(
+        'server.id', ondelete='CASCADE'), primary_key=True)
 )
 
 
 class Usage(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     server_id = db.Column(db.Integer(), db.ForeignKey(
-        'server.id'), ondelete='CASCADE')
-    record_date = db.Column(db.DateTime(), nullable=False)
+        'server.id'))
+    server = db.relationship('Server', backref=db.backref(
+        'usage_log', cascade='all, delete-orphan'))
     cpu_usage = db.Column(db.Integer(), nullable=False)
     memory_usage = db.Column(db.Integer(), nullable=False)
     instance_usage = db.Column(db.Integer(), nullable=False)
+    note = db.Column(db.Text(), nullable=True)
+    record_date = db.Column(db.DateTime(), nullable=False)
