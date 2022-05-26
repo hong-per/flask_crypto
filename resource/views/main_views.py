23947a1ce362373fd54247b09b7c20bfff6b2ca4
hong-per <nunji926@gmail.com>
2022-05-27 00:15:17 +0900

    region list reder

--- resource/views/main_views.py
+++ resource/views/main_views.py
@@ -1,8 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+
+from resource.models import Region
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
 
 @bp.route('/')
 def index():
-    return 'Index'
+    region_list = Region.query.order_by(Region.name.desc())
+    return render_template('region/region_list.html', region_list=region_list)
