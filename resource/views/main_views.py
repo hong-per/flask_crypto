9f987bd92976add820ca6f5fcaeadb816c1ecfe1
hong-per <nunji926@gmail.com>
2022-05-28 23:50:16 +0900

    revise index in main_views

--- resource/views/main_views.py
+++ resource/views/main_views.py
@@ -7,5 +7,5 @@ bp = Blueprint('main', __name__, url_prefix='/')
 
 @bp.route('/')
 def index():
-    region_list = Region.query.order_by(Region.name.desc())
-    return render_template('region/region_list.html', region_list=region_list)
+    regions = Region.query.all()
+    return render_template('base.html', regions=regions)
