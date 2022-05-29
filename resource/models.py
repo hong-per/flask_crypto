158743502ac35a5675feb9f89d100c4b712fdce2
hong-per <nunji926@gmail.com>
2022-05-29 23:36:50 +0900

    app num limit on user password

--- resource/models.py
+++ resource/models.py
@@ -5,7 +5,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False, unique=True)
     email = db.Column(db.String(100), nullable=False, unique=True)
-    password = db.Column(db.String(), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
 
 
 class Region(db.Model):
