445ed9acb29cd1dbd837e1a994eda19b4c089982
hong-per <nunji926@gmail.com>
2022-05-29 23:36:14 +0900

    temporary csfr key

--- config.py
+++ config.py
@@ -5,3 +5,6 @@ BASE_DIR = os.path.dirname(__file__)
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(
     os.path.join(BASE_DIR, 'app.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# temporary setting
+SECRET_KEY = "dev"
