ceb85a172fd6a5a07a62c26a4b7a05e9f81c0894
hong-per <nunji926@gmail.com>
2022-05-29 23:37:51 +0900

    add auth_views in init

--- resource/__init__.py
+++ resource/__init__.py
@@ -18,7 +18,8 @@ def create_app():
     from . import models
 
     # Blueprint
-    from .views import main_views
+    from .views import main_views, auth_views
     app.register_blueprint(main_views.bp)
+    app.register_blueprint(auth_views.bp)
 
     return app
