3d62af27065c38120cba650fee9a0e7209ed684e
hong-per <nunji926@gmail.com>
2022-05-31 00:05:21 +0900

    add region method in region_view

--- resource/views/region_views.py
+++ resource/views/region_views.py
@@ -5,8 +5,9 @@ from resource.models import Region, Server, Usage
 bp = Blueprint('region', __name__, url_prefix='/region')
 
 
-# @bp.route('/<int:region_id>/')
-# def region(region_id):
-#     regions = Region.query.all()
-#     target_region = Region.query.get_or_404(region_id)
-#     return render_template('region/region_detail.html', regions=regions, target_region=target_region)
+@bp.route('/<int:region_id>/')
+def region(region_id):
+    regions = Region.query.all()
+    region = Region.query.get_or_404(region_id)
+    servers = Server.query.filter_by(region_id=region_id)
+    return render_template('region/region_detail.html', regions=regions, region=region, servers=servers)
