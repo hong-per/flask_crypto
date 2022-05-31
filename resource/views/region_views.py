ba7252307acd21783496c650b54575e81ebc4ba0
hong-per <nunji926@gmail.com>
2022-06-01 00:22:20 +0900

    add server create form on region view

--- resource/views/region_views.py
+++ resource/views/region_views.py
@@ -2,11 +2,14 @@ from flask import Blueprint, url_for, render_template, flash, request
 
 from resource.models import Region, Server, Usage
 
+from resource.forms import ServerCreateForm
+
 bp = Blueprint('region', __name__, url_prefix='/region')
 
 
 @bp.route('/<int:region_id>/')
-def region(region_id):
+def detail(region_id):
+    # form = ServerCreateForm()
     regions = Region.query.all()
     region = Region.query.get_or_404(region_id)
     servers = Server.query.filter_by(region_id=region_id)
