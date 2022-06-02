12e84daf6e717b5410c8964ad6c9b2c2e6a15413
hong-per <nunji926@gmail.com>
2022-06-03 00:27:09 +0900

    create server api

--- resource/views/region_views.py
+++ resource/views/region_views.py
@@ -1,16 +1,23 @@
 from flask import Blueprint, url_for, render_template, flash, request
-
+from werkzeug.utils import redirect
+from resource import db
 from resource.models import Region, Server, Usage
-
 from resource.forms import ServerCreateForm
 
 bp = Blueprint('region', __name__, url_prefix='/region')
 
 
-@bp.route('/<int:region_id>/')
+@bp.route('/<int:region_id>/', methods=('GET', 'POST'))
 def detail(region_id):
-    # form = ServerCreateForm()
+    form = ServerCreateForm()
     regions = Region.query.all()
     region = Region.query.get_or_404(region_id)
     servers = Server.query.filter_by(region_id=region_id)
-    return render_template('region/region_detail.html', regions=regions, region=region, servers=servers)
+
+    if request.method == 'POST' and form.validate_on_submit():
+        server = Server(region_id=region_id, host=form.host.data, cpu=form.cpu.data,
+                        memory=form.memory.data, instance=form.instance.data)
+        db.session.add(server)
+        db.session.commit()
+
+    return render_template('region/region_detail.html', regions=regions, region=region, servers=servers, form=form)
