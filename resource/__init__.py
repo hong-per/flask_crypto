7e66a61dca0307939a6239a711388fed08602fdb
hong-per <nunji926@gmail.com>
2022-06-04 23:27:22 +0900

    add server view blueprint

--- resource/__init__.py
+++ resource/__init__.py
@@ -18,9 +18,10 @@ def create_app():
     from . import models
 
     # Blueprint
-    from .views import main_views, auth_views, region_views
+    from .views import main_views, auth_views, region_views, server_views
     app.register_blueprint(main_views.bp)
     app.register_blueprint(auth_views.bp)
     app.register_blueprint(region_views.bp)
+    app.register_blueprint(server_views.bp)
 
     return app
