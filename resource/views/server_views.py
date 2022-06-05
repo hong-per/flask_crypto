49149c5f1974cf1b5fefb00fc603f68a947f06f9
hong-per <nunji926@gmail.com>
2022-06-05 23:18:39 +0900

    add server create method in server view

--- resource/views/server_views.py
+++ resource/views/server_views.py
@@ -7,6 +7,21 @@ from resource.forms import ServerForm
 bp = Blueprint('server', __name__, url_prefix='/server')
 
 
+@bp.route('create/region<int:region_id>/', methods=('GET', 'POST'))
+def create(region_id):
+    form = ServerForm()
+    region = Region.query.get_or_404(region_id)
+
+    if request.method == 'POST' and form.validate_on_submit():
+        server = Server(region_id=region_id, host=form.host.data, cpu=form.cpu.data,
+                        memory=form.memory.data, instance=form.instance.data)
+        db.session.add(server)
+        db.session.commit()
+        return redirect(url_for('region.detail', region_id=region_id))
+
+    return render_template('server/add_server.html', region=region, form=form)
+
+
 @bp.route('update/<int:server_id>/', methods=('GET', 'POST'))
 def update(server_id):
     form = ServerForm()
