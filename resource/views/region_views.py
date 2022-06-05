b0661f736420dbc4af923bd7726683d52e3558b5
hong-per <nunji926@gmail.com>
2022-06-05 11:36:51 +0900

    modify region view

--- resource/views/region_views.py
+++ resource/views/region_views.py
@@ -2,18 +2,19 @@ from flask import Blueprint, url_for, render_template, flash, request
 from werkzeug.utils import redirect
 from resource import db
 from resource.models import Region, Server, Usage
-from resource.forms import ServerCreateForm
+from resource.forms import ServerForm
 
 bp = Blueprint('region', __name__, url_prefix='/region')
 
 
 @bp.route('/<int:region_id>/', methods=('GET', 'POST'))
 def detail(region_id):
-    form = ServerCreateForm()
+    form = ServerForm()
     regions = Region.query.all()
     region = Region.query.get_or_404(region_id)
     servers = Server.query.filter_by(region_id=region_id)
 
+    # create server
     if request.method == 'POST' and form.validate_on_submit():
         server = Server(region_id=region_id, host=form.host.data, cpu=form.cpu.data,
                         memory=form.memory.data, instance=form.instance.data)
