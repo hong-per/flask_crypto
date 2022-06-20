e7f91752830b08806e2b744da9e04464761cb630
hong-per <nunji926@gmail.com>
2022-06-20 23:28:22 +0900

    create test_models.py

--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -1,31 +1,9 @@
 from resource.models import User, Region
+from tests.conftest import new_user
 
 
-def test_new_user():
-    # given
-    data = {
-        'name': 'test_user',
-        'email': 'test@test.com',
-        'password': 123456
-    }
-
-    # when
-    user = User(data['name'], data['email'], data['password'])
-
-    # then
-    assert user.name == 'test_user'
-    assert user.email == 'test@test.com'
-    assert user.password == 123456
-
-
-# def test_new_region():
-#     # given
-#     data = {
-#         'name': 'test_region'
-#     }
-
-#     # when
-#     region = Region(data['name'])
-
-#     # then
-#     assert region.name == 'test_region'
+def test_new_user_with_fixture(new_user):
+    
+    assert new_user.name == 'test_user'
+    assert new_user.email == 'test@test.com'
+    assert new_user.password == 123456
