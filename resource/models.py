228ef2bd6634385024bdd4bf7a92cfaa64402105
hong-per <nunji926@gmail.com>
2022-06-24 00:20:03 +0900

    fix description on models

--- resource/models.py
+++ resource/models.py
@@ -60,4 +60,4 @@ class Usage(db.Model):
 # use date format to save record date
 # ex) date(2022, 6, 1)
 # user datetime format when searching
-# ex) Usage.query.filter_by(record_date=datetime(2022, 6, 1, 0, 0)).first()
+# ex) Usage.query.filter_by(record_date=datetime.datetime(2022, 6, 1, 0, 0)).first()
