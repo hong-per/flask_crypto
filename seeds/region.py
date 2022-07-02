5205ff2d7bdff5ddade0083d80b7b2f62c7103ca
hong-per <nunji926@gmail.com>
2022-07-03 00:10:55 +0900

    fix region seed

--- seeds/region.py
+++ seeds/region.py
@@ -1,30 +1,16 @@
 from resource import db
+from resource.models import Region
 from flask_seeder import Seeder, Faker, generator
-
-# SQLAlchemy database model
-
-
-class Region(db.Model):
-    def __init__(self, id=None, name=None):
-        self.id = id
-        self.name = name
-
-
-    def __str__(self):
-        return "ID=%d, Name=%s" % (self.id, self.name)
-
-# All seeders inherit from Seeder
 
 
 class RegionSeeder(Seeder):
 
     # run() will be called by Flask-Seeder
     def run(self):
-        # Create a new Faker and tell it how to create User objects
+        # Create a new Faker and tell it how to create Region objects
         faker = Faker(
             cls=Region,
             init={
-                "id": generator.Sequence(start=6),
                 "name": generator.Name()
             }
         )
