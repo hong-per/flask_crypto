c05514d7483aa0e5d3c13d189975e331a22b61cd
hong-per <nunji926@gmail.com>
2022-07-09 23:59:30 +0900

    revise main views

--- resource/views/main_views.py
+++ resource/views/main_views.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, render_template
-
 from resource.models import Region
+import pandas as pd
+import json
+import plotly
+import plotly.express as px
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
@@ -8,4 +11,16 @@ bp = Blueprint('main', __name__, url_prefix='/')
 @bp.route('/')
 def index():
     regions = Region.query.all()
-    return render_template('base.html', regions=regions)
+    df = pd.DataFrame({
+        'Fruit': ['Apples', 'Oranges', 'Bananas', 'Apples', 'Oranges',
+                  'Bananas'],
+        'Amount': [4, 1, 2, 2, 4, 5],
+        'City': ['SF', 'SF', 'SF', 'Montreal', 'Montreal', 'Montreal']
+    })
+
+    fig = px.bar(df, x='Fruit', y='Amount', color='City',
+                 barmode='group')
+
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return render_template('dashboard.html', regions=regions, graphJSON=graphJSON)
