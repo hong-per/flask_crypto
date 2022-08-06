9fb768d5eaebeb6ab8bb92e18ed435fb00d2c740
hong-per <nunji926@gmail.com>
2022-08-06 23:44:24 +0900

    trend page refactoring

--- resource/views/trend_views.py
+++ resource/views/trend_views.py
@@ -24,64 +24,25 @@ def main(region_id):
     return render_template('trend/trend.html', regions=regions, region=region, line=line)
 
 
-def get_logs_by_region(region):
-    return sum([server.logs for server in region.servers], [])
-
-
-def get_dates_by_region(region):
-    logs = get_logs_by_region(region)
-    return list(set([log.record_date for log in logs]))
-
-
 def get_cpu_usages_by_dates(region):
-    logs = get_logs_by_region(region)
-    dates = get_dates_by_region(region)
-    dates.sort()
-
-    cpu_usages = []
-    for date in dates:
-        date_logs = list(filter(lambda x: x.record_date == date, logs))
-        cpu_usages.append(
-            reduce(lambda x, y: x + y, [log.cpu_usage for log in date_logs]))
-
     df = pd.DataFrame({
-        'date': dates,
-        'usage': cpu_usages
+        'date': region.dates(),
+        'usage': region.cpu_usage_by_date()
     })
     return px.line(df, x="date", y="usage", title=f"cpu usage trend in {region.name}")
 
 
 def get_memory_usages_by_dates(region):
-    logs = get_logs_by_region(region)
-    dates = get_dates_by_region(region)
-    dates.sort()
-
-    memory_usages = []
-    for date in dates:
-        date_logs = list(filter(lambda x: x.record_date == date, logs))
-        memory_usages.append(
-            reduce(lambda x, y: x + y, [log.memory_usage for log in date_logs]))
-
     df = pd.DataFrame({
-        'date': dates,
-        'usage': memory_usages
+        'date': region.dates(),
+        'usage': region.memory_usage_by_date()
     })
     return px.line(df, x="date", y="usage", title=f"memory usage trend in {region.name}")
 
 
 def get_storage_usages_by_dates(region):
-    logs = get_logs_by_region(region)
-    dates = get_dates_by_region(region)
-    dates.sort()
-
-    storage_usages = []
-    for date in dates:
-        date_logs = list(filter(lambda x: x.record_date == date, logs))
-        storage_usages.append(
-            reduce(lambda x, y: x + y, [log.storage_usage for log in date_logs]))
-
     df = pd.DataFrame({
-        'date': dates,
-        'usage': storage_usages
+        'date': region.dates(),
+        'usage': region.storage_usage_by_date()
     })
     return px.line(df, x="date", y="usage", title=f"storage usage trend in {region.name}")
