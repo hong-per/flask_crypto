e4484ebaf237b00f632d4486573220b487015ca7
hong-per <nunji926@gmail.com>
2022-08-07 22:52:13 +0900

    delete unnecessary lib in test models

--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -1,9 +1,5 @@
-from resource.models import User, Region
-from tests.conftest import new_user
-
-
 def test_new_user_with_fixture(new_user):
-    
+
     assert new_user.name == 'test_user'
     assert new_user.email == 'test@test.com'
     assert new_user.password == 123456
